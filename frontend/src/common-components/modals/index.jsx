0c18bc81c836d66bc6cce9d8256d8da7e56652bc
TIMUR <otto@MacBook-Air-Timur.local>
2023-02-22 00:32:08 +0300

    Add new files

---
+++ frontend/src/common-components/modals/index.jsx
@@ -0,0 +1,33 @@
+import { useSelector } from 'react-redux';
+import { selectModalState } from '../../slices/modalSlice.js';
+import Add from './Add.jsx';
+import Remove from './Remove.jsx';
+import Rename from './Rename.jsx';
+
+export const modalTypes = {
+  ADDING: 'adding',
+  REMOVING: 'removing',
+  RENAMING: 'renaming',
+};
+
+const modals = {
+  adding: Add,
+  removing: Remove,
+  renaming: Rename,
+};
+
+const getModal = (modalType) => modals[modalType];
+
+const ChannelModal = () => {
+  const { isOpened, type } = useSelector(selectModalState);
+
+  if (!isOpened) {
+    return null;
+  }
+
+  const Component = getModal(type);
+
+  return <Component />;
+};
+
+export default ChannelModal;
